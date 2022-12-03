ee6514fea548e1d4f4e2bce6af3b49d2e38be925
Zeynalbey <zeynaleg@code.edu.az>
2022-12-04 01:50:45 +0400

    SubNavbar ready.

---
+++ DemoApp/DemoApplication/ViewModels/Admin/Navbar/NavbarListViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DemoApplication.ViewModels.Admin.Navbar
+{
+    public class NavbarListViewModel
+    {
+        public NavbarListViewModel(int id, string name, bool isMain, bool isHeader, bool isFooter)
+        {
+            Id = id;
+            Name = name;
+            IsMain = isMain;
+            IsHeader = isHeader;
+            IsFooter = isFooter;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsMain { get; set; }
+        public bool IsHeader { get; set; }
+        public bool IsFooter { get; set; }
+
+    }
+}
