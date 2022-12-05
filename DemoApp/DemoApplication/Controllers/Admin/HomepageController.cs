42cf8bca1cc472f9b5d11f7d02d99e92deab1043
Zeynalbey <zeynaleg@code.edu.az>
2022-12-05 12:41:58 +0400

    Homepage and viewImport added

---
+++ DemoApp/DemoApplication/Controllers/Admin/HomepageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApplication.Controllers.Admin
+{
+    public class HomepageController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
